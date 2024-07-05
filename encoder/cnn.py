f5a00a41dba9caeb07c60b644de245ba39fd3e9c
Yushuo Feng <66173381+FullToker@users.noreply.github.com>
2024-07-05 11:44:30 +0200

    add cnn fot test, need to combine the encoder with clf

---
+++ encoder/cnn.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from autoencoder import Encoder
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print("You are using the following device: ", device)
+encoder = Encoder().to(device)
+encoder.load_state_dict(torch.load("./encoder/save/encoder.pth"))
+
+# load the dataset
+train_data = torch.load("./encoder/save/train_argu.pth")
+test_data = torch.load("./encoder/save/test_argu.pth")
+
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+for data in train_loader:
+    data = data.to(device)
+for data in test_loader:
+    data = data.to(device)
